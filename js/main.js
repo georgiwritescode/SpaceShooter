c18a2ac7f27357dd665a162e2db4ac7924f9831b
ivanov.joe93@gmail.com <ma1kat13mujw3>
2016-03-27 17:28:12 +0300

    finally fixed the movement does not overshoots target

--- js/main.js
+++ js/main.js
@@ -4,8 +4,9 @@ var game = new Phaser.Game(1200, 640, Phaser.AUTO, null, {
 
 var sniper;
 var rockets;
-var fireRate = 40;
+var fireRate = 100;
 var nextFire = 0;
+var tween;
 
 function preload() {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -20,25 +21,27 @@ function create() {
 
     //sniper sprites and animations
     sniper = game.add.sprite(600,320,'sniper');
+
     var walk = sniper.animations.add('walk');
     sniper.animations.play('walk',8,true);
+
     game.physics.enable(sniper,Phaser.Physics.ARCADE);
     sniper.anchor.set(0.5, 0.5);
+
     sniper.body.collideWorldBounds = true;
-    sniper.body.allowRotation = false;
-    /*
-    var tween = this.game.add.tween(this.sniper.body).to({
-        x: game.input.activePointer.rightButton.x,
-        y: game.input.activePointer.rightButton.y
-    }, 1000, Phaser.Easing.Linear.None, true);
-    */
+    sniper.body.allowRotation = true;
+
+
+
     rockets = game.add.group();
     rockets.enableBody = true;
     rockets.physicsBodyType = Phaser.Physics.ARCADE;
+
     rockets.createMultiple(10000, 'rocket');
     rockets.setAll('checkWorldBounds', true);
-    rockets.setAll('outOfBoundsKill', false);
-
+    rockets.setAll('outOfBoundsKill', true);
+    //rockets.checkWorldBounds = true;
+    //rockets.body.collideWorldBounds = true;
 }
 function update() {
     // add math.pi/2 so the front follows the mouse pointer
@@ -51,14 +54,25 @@ function update() {
 }
 
 function fire() {
-    if (game.time.now > nextFire){
+
+    if (game.time.now > nextFire && rockets.countDead() > 0){
         nextFire = game.time.now + fireRate;
         var rocket = rockets.getFirstDead();
         rocket.reset(sniper.x - 8 , sniper.y - 8);
+        game.physics.arcade.moveToPointer(rocket, 300);
     }
+
+    console.log('shooting')
 }
 
 function moveToPosition(){
-    game.physics.arcade.moveToPointer(sniper,100);
+   // var pos = game.physics.arcade.moveToPointer(sniper,100);
+    //game.physics.moveToXY(this.game.activePointer = game.width);
+   // game.physics.activePointer.circle.contains(sniper.x,sniper.y);
+    var pos = game.input.activePointer.position;
+    tween = this.game.add.tween(sniper).to({
+        x: pos.x,
+        y: pos.y
+    }, 1000, Phaser.Easing.Linear.None, true);
 
 }
